03611a20cc1d68457887a78ddc831aecb8a4f0c7
Vishnu Ramineni <vishnu.ramineni.05@gmail.com>
2026-02-04 21:49:30 -0600

    updated workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+      base: './', // This line is already present and should be retained
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
